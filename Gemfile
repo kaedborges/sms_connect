14ab04cf532571e7522c50a57d9ce30d774eea8b
kaedborges <edvander.borges@next.co.ao>
2022-10-19 16:00:55 +0100

    Add client-rest

--- Gemfile
+++ Gemfile
@@ -44,6 +44,7 @@ gem "bootsnap", require: false
 
 gem 'devise'
 gem 'httparty'
+gem 'rest-client'
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
--- app/services/send_sms_backbone_services.rb
+++ app/services/send_sms_backbone_services.rb
@@ -2,16 +2,16 @@ class SendSmsBackboneServices
     include HTTParty
     base_uri '52.30.114.86:8080/mimosms/v1/message'
 
+    attr_accessor :payload
     def initialize
-
+        @payload
     end
-
     def send_sms(number,text,sender)
         payload = {
             :sender => sender,
             :recipients => number,
             :text => text
         }
-        self.class.post("/send?token=", payload)
+        self.class.post("/send?token=2ae7b2e6b39ea7e9689784dff8789488927878988", payload)
     end
 end
