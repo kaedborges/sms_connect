51275430ebeae3c05f90034d3b3589618e78a261
kaedborges <edvander.borges@next.co.ao>
2022-10-20 10:57:54 +0100

    Add manager from device

--- db/migrate/20221018130718_add_status_manager_and_index.rb
+++
@@ -1,6 +0,0 @@
-class AddStatusManagerAndIndex < ActiveRecord::Migration[7.0]
-  def change
-    add_column :managers, :status, :integer
-    add_index :managers, :authentication_token, unique: true
-  end
-end
