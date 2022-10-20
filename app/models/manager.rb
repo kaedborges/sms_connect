c25aae235b52b09ade3236e5da8ee48b5bafaa92
kaedborges <edvander.borges@next.co.ao>
2022-10-20 10:58:47 +0100

    Add manager from device

--- app/models/manager.rb
+++ app/models/manager.rb
@@ -1,6 +1,8 @@
 class Manager < ApplicationRecord
-  #acts_as_token_authenticatable
-  #devise :database_authenticatable, :recoverable, :rememberable, :validatable
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
 
   enum status: { inactive: 0, active: 1}, _default: :inactive
 end
