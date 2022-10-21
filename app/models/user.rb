f6fe16de058ca6261c28b71f1110b4b39acceb53
kaedborges <edvander.borges@next.co.ao>
2022-10-21 21:08:49 +0100

    add validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,13 @@ class User < ApplicationRecord
   has_secure_password
 
   enum status: { inactive: 0, active: 1}, _default: :inactive
+  validates :cellphone, presence: { message: 'CELLPHONE_IS_REQUIRED' }
+  validates :cellphone, uniqueness: { message: 'CELLPHONE_IS_NOT_AVAILABLE' }
+  validates :cellphone, phone: {types: [:mobile], countries: :ao, message: 'INVALID_PHONE_NUMBER'}
+  validates :name, presence: { message: 'NAME_IS_REQUIRED' }
+
+  def create_otp
+    self.otp = SecureRandom.random_number(10**6).to_s.rjust(6, '0')
+    self.save
+  end
 end
