f348842c92e9ca86a5240e0683cb7fb17ca1ae3c
kaedborges <edvander.borges@next.co.ao>
2022-10-21 18:31:01 +0100

    complite authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,48 +2,31 @@ class ApplicationController < ActionController::API
     rescue_from ActiveRecord::RecordNotFound, with: :not_found
     private
     SECRET_KEY = 'secrete2022Teste'
+
     def encode_token(payload, exp = 24.hours.from_now)
         payload[:exp] = exp.to_i
         JWT.encode(payload, SECRET_KEY)
     end
 
-    def decode_token
-        auth_header = request.headers['Authorization']
-        render :unauthorized, status: :unauthorized and return if auth_header.nil?
-
-        if auth_header
-            token = auth_header.split(' ').last
-            puts token
-            begin
-                JWT.decode(token, SECRET_KEY, true, algorithm: 'HS256')
-            rescue JWT::DecodeError
-                nil
-            end
-        end
+    def decode_token(token)
+        decoded = JWT.decode(token, SECRET_KEY)[0]
+        HashWithIndifferentAccess.new decoded
     end
 
     def validate_manager_token
-        token = decode_token
-        puts token
-        if token
-            manager_id = token[0]['user_id']
-            @current_manager = Manager.find_by(id: manager_id)
-        else
+        header = request.headers['Authorization']
+        header = header.split(' ').last if header
+        begin
+            @decoded = decode_token(header)
+            puts @decoded['user_id']
+            @current_manager = Manager.find_by(id: @decoded['user_id'])
+            # @current_user = Manager.find(decoded[:user_id])
+        rescue ActiveRecord::RecordNotFound => e
+            render :unauthorized, status: :unauthorized
+        rescue JWT::DecodeError => e
             render :unauthorized, status: :unauthorized
         end
     end
-
-    # def validate_manager_token
-    #     token = request.header['Authorization']
-    #     api_key = request.header['API_KEY']
-    #     rende :unauthorized, status: :unauthorized and return if api_key.nil? || api_key != "KD sms_connect"
-    #     rende :unauthorized, status: :unauthorized and return if token.nil?
-    #
-    #     @manager = Manager.find_by(authentication_token: token)
-    #     render :unauthorized, status: :unauthorized and return if @manager.nil?
-    #
-    #     @manager
-    # end
 
     def random_password(length = 12)
         chars = ('0'..'9').to_a + ('A'..'Z').to_a + ('a'..'z').to_a
