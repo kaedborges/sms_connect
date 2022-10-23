f19e3dbc1beec0eefe91eed47bbd698d4c9f5a3f
kaedborges <edvander.borges@next.co.ao>
2022-10-23 13:36:40 +0100

    Adding route to credits to manager

--- config/routes.rb
+++ config/routes.rb
@@ -23,6 +23,12 @@ Rails.application.routes.draw do
       patch         '/:id/lock'                                                   => 'managers#lock'
       patch         '/:id/onlock'                                                 => 'managers#unlock'
 
+      #Credits
+      get           '/credits/global'                                             => 'credits#global'
+      get           '/credits/user/:id'                                           => 'credits#user_credits'
+      post          '/credits/user/:id/add'                                       => 'credits#user_add'
+      post          '/credits/user/:id/remove'                                    => 'credits#user_remove'
+
 
     end
     namespace :users do
