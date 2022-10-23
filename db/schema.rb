9cc87e57bc5eb3f98fef83611df1ec7d6b2903c3
kaedborges <edvander.borges@next.co.ao>
2022-10-23 23:05:51 +0100

    adding sender model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_23_121435) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_23_214217) do
   create_table "credits_histories", force: :cascade do |t|
     t.integer "balance"
     t.string "operation"
@@ -27,6 +27,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_23_121435) do
     t.string "token_auth"
     t.string "cellphone"
     t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "senders", force: :cascade do |t|
+    t.string "name"
+    t.integer "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
